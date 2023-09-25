ae649e2a03990cf26e0fbbd433e9e55976b9d900
a20150091 <131031875+a20150091@users.noreply.github.com>
2023-09-25 13:13:20 -0500

    Add files via upload

---
+++ preguntas.js
@@ -0,0 +1,29 @@
+var nombre = prompt("Escribe tu nombre");
+var respuesta1 = prompt("1. ¿Cómo se llama al primer día de la Primavera?\nA: Primer Día de la Primavera\nB: Equinoccio de Primavera\nC: Equinoccio de Invierno ");
+
+
+var respuesta2 = prompt("2. ¿Qué sucede en el Polo Norte cuando llega la Primavera?\nA: 6 meses de luz diurna ininterrumpida\nB: 6 meses de oscuridad inimterrumpida\nC: 6 meses de alternancia de luz y oscuridad ")
+
+var respuesta3 = prompt("3. ¿En qué mes empieza la Primavera en el Hemisferio Norte?\nA: Setiembre\nB: Junio\nC: Marzo")
+
+var respuesta4 = prompt("3. ¿Qué es la Fiesta de la Primavera en China?\nA: La Navidad\nB: El Año Nuevo\nC: Fiestas Patrias de China");
+
+var parseInt(puntaje)= 0
+
+if (respuesta1 == "B"){
+    var puntaje = puntaje + 25;
+}
+
+if (respuesta2 == "A"){
+    var puntaje = puntaje + 25;
+}
+
+if (respuesta3 == "C"){
+    var puntaje = puntaje + 25,
+}
+
+if (respuesta4 == "B"){
+
+}
+
+
